a81717c1214e00f4cfe5144ac2fe5f4d42ee421d
chrillah <ch.ronnberg@gmail.com>
2023-03-20 15:13:22 +0100

    react files generated and first component added

---
+++ my-project/src/components/FunctionalComponent.js
@@ -0,0 +1,12 @@
+function FunctionalComponent(){
+    const s = "hell-world"
+    return(
+        <div id={s}>
+            <h1>Hell World!</h1>
+            <p>Det här bir 8 = {4 + 4}</p>
+        </div>
+
+    )
+}
+
+export default FunctionalComponent
