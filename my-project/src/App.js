46d79df3e028bcbfb40413e3c6ce9852020d6ec6
chrillah <ch.ronnberg@gmail.com>
2023-04-03 16:11:44 +0200

    added

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,14 +1,18 @@
 import './App.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+// import Home from './pages/Home'
+// import About from './pages/About'
+// import Contact from './pages/Contact'
 
-import Root from './components/Root'
+// import Root from './components/Root'
 
-import {
-    createHashRouter,
-    RouterProvider
-} from 'react-router-dom'
+// import {
+//     createHashRouter,
+//     RouterProvider
+// } from 'react-router-dom'
+
+import {useState} from 'react'
+import SomeComponent from './components/SomeComponent'
+import SomeContext from './components/SomeContext'
 
 // import Router from './components/Router'
 
@@ -34,19 +38,24 @@ import {
 // }
 
 function App() {
-    const router = createHashRouter([
-        {
-            children: [
-                { element: <Home />, path: '/' },
-                { element: <About />, path: '/about/:userName' },
-                { element: <Contact />, path: '/contact' }
-            ],
-            element: <Root />
-        }
-    ])
-    return <RouterProvider router={router} />
+    const [someValue, setSomeValue] = useState(1)
+    return (
+        <SomeContext.Provider value={{someValue, setSomeValue}}>
+            <SomeComponent />
+        </SomeContext.Provider>
+    )
+    // const router = createHashRouter([
+    //     {
+    //         children: [
+    //             { element: <Home />, path: '/' },
+    //             { element: <About />, path: '/about/:userName' },
+    //             { element: <Contact />, path: '/contact' }
+    //         ],
+    //         element: <Root />
+    //     }
+    // ])
+    // return <RouterProvider router={router} />
 
-    // <Router />
 }
 
 export default App
